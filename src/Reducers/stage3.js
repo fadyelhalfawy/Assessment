2724f8c665052823d9ab2db543ae3de7a7f349f9
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-08-28 18:18:40 +0300

    Update the stage 3 by removing unused lines.

--- src/Reducers/stage3.js
+++ src/Reducers/stage3.js
@@ -1,27 +1,9 @@
 import PaginatedQuestions from './pagination';
 
-const questions = [
-  "What is your name?",
-  "What is your age?",
-  "What is your favorite color?",
-  "What is your favorite food?",
-  "What is your hobby?",
-  "Where do you live?",
-  "What is your profession?",
-  "What are your goals?",
-  "What is your favorite movie marvel?",
-  "What is your favorite position in the field?",
-  "What is your favorite player?",
-  "What is your favorite sport?",
-  "What is your favorite money?",
-];
-
 const Stage3 = () => {  
   
   return (
-      <PaginatedQuestions
-        questions={questions}
-      />
+      <PaginatedQuestions/>
   );
 };
 
