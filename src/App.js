2dd6a7ace3a1ff6e7ada1e82bb905a0af4b21df6
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-08-28 18:16:57 +0300

    Update App file by adding the fourth stage.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { useSelector } from 'react-redux';
 import Stage1 from './Reducers/stage1';
 import Stage2 from './Reducers/stage2';
 import Stage3 from './Reducers/stage3';
+import Stage4 from './Reducers/stage4';
 
 const App = () => {
   const stage = useSelector((state) => state.form.stage);
-  const form = useSelector((state) => state.form);
-  console.log(form);
   
   return (
     <div>
       {stage === 1 && <Stage1 />}
       {stage === 2 && <Stage2 />}
       {stage === 3 && <Stage3 />}
+      {stage === 4 && <Stage4 />}
     </div>
   );
 }
