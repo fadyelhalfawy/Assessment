b75a6bd14d95f416f8907a3fd3b930ef3998a15f
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-08-28 18:17:37 +0300

    Make the questions in a file.

---
+++ src/Reducers/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+    "What is your name?",
+    "What is your age?",
+    "What is your favorite color?",
+    "What is your favorite food?",
+    "What is your hobby?",
+    "Where do you live?",
+    "What is your profession?",
+    "What are your goals?",
+    "What is your favorite movie marvel?",
+    "What is your favorite position in the field?",
+    "What is your favorite player?",
+    "What is your favorite sport?",
+    "What is your favorite money?",
+  ];
+
+  export default questions;
